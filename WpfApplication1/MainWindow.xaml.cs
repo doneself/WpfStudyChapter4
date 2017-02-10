7e3651a67cdfd0aa29dcd9253fac8eb7b2ded6bf
lok <lok.ng@hdcircles.com>
2017-02-10 11:26:41 +0800

    Lok first commit--DependencyProperty TimeProperty Create.

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +21,59 @@ namespace WpfApplication1
     /// </summary>
     public partial class MainWindow : Window
     {
+        public delegate void SetTimeDelegate(DateTime time);
+        public static DependencyProperty TimeProperty;
+        private Timer myTimer;
+
         public MainWindow()
         {
             InitializeComponent();
+            myTimer = new Timer(new TimerCallback(RefreshTime), new AutoResetEvent(false), 0, 1000);
+        }
+
+        public DateTime Time
+        {
+            get
+            {
+                return (DateTime)GetValue(TimeProperty);
+            }
+            set
+            {
+                SetValue(TimeProperty, value);
+            }
+        }
+
+        static bool CalidateTimeValue(object obj)
+        {
+            DateTime dt = (DateTime)obj;
+            if (dt.Year > 1990 && dt.Year < 2200)
+            { return true; }
+            return false;
+        }
+
+        static MainWindow()
+        {
+            FrameworkPropertyMetadata metadata = new FrameworkPropertyMetadata();
+            metadata.Inherits = true;
+            metadata.DefaultValue = DateTime.Now;
+            metadata.AffectsMeasure = true;
+            metadata.PropertyChangedCallback += OnTimerPropertyChanged;
+            TimeProperty = DependencyProperty.Register("Timer", typeof(DateTime), typeof(MainWindow), metadata, CalidateTimeValue);
+        }
+
+        static void OnTimerPropertyChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+        }
+
+        private void RefreshTime(Object stateInfo)
+        {
+            SetTimeDelegate d = new SetTimeDelegate(SetTimeProperty);
+            this.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Send, d, DateTime.Now);
+        }
+
+        private void SetTimeProperty(DateTime dt)
+        {
+            this.Time = dt;
         }
     }
 }

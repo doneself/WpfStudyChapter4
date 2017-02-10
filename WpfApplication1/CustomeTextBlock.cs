a2df882120ccc20b5882208dde641cca1878cb3a
lok <lok.ng@hdcircles.com>
2017-02-10 14:59:35 +0800

    Finish

---
+++ WpfApplication1/CustomeTextBlock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApplication1
+{
+    public class CustomeTextBlock:TextBlock
+    {
+        public static DependencyProperty TimeProperty;
+
+        public DateTime Timer
+        {
+            get
+            {
+                return (DateTime)GetValue(TimeProperty);
+            }
+            set
+            {
+                SetValue(TimeProperty, value);
+            }
+        }
+        static CustomeTextBlock()
+        {
+            FrameworkPropertyMetadata metaData = new FrameworkPropertyMetadata();
+            metaData.Inherits = true;
+            TimeProperty = MainWindow.TimeProperty.AddOwner(typeof(CustomeTextBlock));
+            TimeProperty.OverrideMetadata(typeof(CustomeTextBlock), metaData);
+        }
+    }
+}
